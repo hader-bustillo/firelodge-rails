6890d0c53fb37cc9d9ab1592ca78041fd8ed074a
Hader Bustillo <haderbustillo@gmail.com>
2024-01-04 17:34:35 +0700

    Landing UI from nextJS app

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -10,7 +10,20 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+        serif: ["Original", ...defaultTheme.fontFamily.serif],
+      },
+      backgroundImage: {
+        "hero-pattern":
+          "linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(16,185,129,0.9122242647058824) 32%, rgba(16,185,129,0.671327906162465) 66%, rgba(16,185,129,1) 100%)",
+      },
+      dropShadow: {
+        hero: "0px 0px 2px rgba(0, 0, 0, 0.64),0px 0px 5px rgba(0, 10, 134, 0.46),0px 4px 14px rgba(0, 6, 83, 0.56)",
+      },
+      grayscale: {
+        25: "25%",
+        50: "50%",
+        75: "75%",
       },
     },
   },
